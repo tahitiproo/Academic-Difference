8f236e903373639b00eb189e80487442357e6182
Daniil Morozov <daniilmgn0@mail.ru>
2025-09-21 11:55:52 +0300

    checked work of numerical types and standard input output

--- attempt.c
+++ attempt.c
@@ -1,8 +1,20 @@
+// моя первая программа
 #include <stdio.h>
 
 int main(){
-    printf("Hello, World!\n");
-    return 0;
-}
+     /*double d1, d2, d3, d4;
+     float var_f;
+     var_f = 10.0;
 
-// gcc -std=c99 -o outputfile sourcefile.c-
+     d1 = 10.0f;
+     d2 = -7.;
+     d3 = 1e2;
+     d4 = 5e-3;
+
+     int size_float = sizeof(var_f); */
+
+     int value1 = getchar();
+     int value2 = getchar();
+     printf("%c %c\n", value1, value2);
+     return 0;
+}
