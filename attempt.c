afc65b7973794ba25077f17877b713791c8b1ad1
Daniil Morozov <daniilmgn0@mail.ru>
2025-09-22 21:19:45 +0300

    Have been tested format string

--- attempt.c
+++ attempt.c
@@ -2,19 +2,25 @@
 #include <stdio.h>
 
 int main(){
-     /*double d1, d2, d3, d4;
-     float var_f;
-     var_f = 10.0;
+    /*double d1, d2, d3, d4;
+    float var_f;
+    var_f = 10.0;
 
-     d1 = 10.0f;
-     d2 = -7.;
-     d3 = 1e2;
-     d4 = 5e-3;
+    d1 = 10.0f;
+    d2 = -7.;
+    d3 = 1e2;
+    d4 = 5e-3;
 
-     int size_float = sizeof(var_f); */
+    int size_float = sizeof(var_f); 
+    int var_t = -1542;
+    double var_ld = -52412.51354;
+    printf("[%#X]\n",var_t);
+    printf("[%+.2f]\n",var_ld);  */
+    long long var_lli = 0;
+    double var_d = 0;
 
-     int value1 = getchar();
-     int value2 = getchar();
-     printf("%c %c\n", value1, value2);
-     return 0;
+    int count = scanf("%lld %lf", &var_lli, &var_d);
+    printf("res is %d: var_lli is %lld, var_d is %.2f\n", count, var_lli, var_d);
+
+    return 0;
 }
